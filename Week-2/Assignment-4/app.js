757f58e1258a6444c51bce413e54bf25edb58e13
Evelyn <Cyc.evelynchang@gmail.com>
2024-06-26 22:29:39 +0800

    upload assigment-4

---
+++ Week-2/Assignment-4/app.js
@@ -0,0 +1,22 @@
+/*
+Request 1: Click to Change Text.
+When the user clicks on the "Welcome Message" block, change the text to "Have a Good
+Time!".
+*/
+const welcomeMsg = document.getElementById('welcome-msg');
+welcomeMsg.addEventListener('click', () => {
+    welcomeMsg.innerHTML = "Have a Good Time!";
+});
+
+/* 
+Request 2: Click to Show More Content Boxes.
+There are some more content boxes waiting to show. When the user clicks the Call-to-Action
+button, show those hidden content boxes.
+
+Hint: all content boxes are already there, they are just set to display: none at the beginning.
+*/
+const hiddenContent = document.querySelector('.hidden-content-container');
+const botton = document.getElementById('botton');
+botton.addEventListener('click', () => {
+    hiddenContent.style.display = 'block';
+});
