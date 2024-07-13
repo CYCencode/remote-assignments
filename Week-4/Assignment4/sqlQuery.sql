87f9efc349d4beb711aa1499fb40a1b51f32547d
Evelyn <Cyc.evelynchang@gmail.com>
2024-07-13 13:29:02 +0800

    adjust for sql query, backup.sql encode type

--- Week-4/Assignment4/sqlQuery.sql
+++ Week-4/Assignment4/sqlQuery.sql
@@ -4,5 +4,6 @@ JOIN user ON user.id = article.user_id
 WHERE email = "Cyc.evelynchang@gmail.com";
 
 -- Write another SQL statement to select articles from 7th to 12th sorted by id.
-SELECT* FROM assignment.article
-WHERE id BETWEEN 7 AND 12;
+SELECT * FROM assignment.article
+ORDER BY id
+LIMIT 6 OFFSET 6;
